d59bb32228ba6753ea53b791c57e4a71b47eb041
Thijn <thijnkroon@gmail.com>
2024-01-27 09:59:47 +0100

    feat: add cooldown and rate limit cmd options

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -16,15 +16,70 @@ export interface CommandProps {
 
 export type CommandOptions = {
   /**
-   * When set to `true`, this command will be registered only in the `TEST_GUILD` defined in the .env
+   * When set to `true`, this command will be registered only in the `TEST_GUILD` defined in the .env.
    * @default false
    */
   private?: boolean;
   /**
-   * When set to `true`, only admins in the server will be allowed to use this command
+   * When set to `true`, only admins in the server will be allowed to use this command.
    * @default false
    */
   adminOnly?: boolean;
+  /**
+   * When defined, this command will be on cooldown for the given number of seconds.
+   *
+   * On a per-user basis by default, which can be changed to guild-wide by setting the scope to `guild`.
+   * @default undefined
+   *
+   * @see `rateLimit` option for more fine-tuned control.
+   */
+  cooldown?:
+    | number
+    | {
+        /**
+         * The number of seconds this command will be on cooldown for.
+         * @default 5
+         */
+        seconds?: number;
+        /**
+         * The scope of the cooldown.
+         * @default user
+         *
+         * @values
+         * `user` - The cooldown will be on a per-user basis.
+         *
+         * `guild` - The cooldown will be on a per-guild basis.
+         */
+        scope?: 'user' | 'guild';
+      };
+  /**
+   * When defined, this command will be rate limited on a per-user basis.
+   *
+   * On a per-user basis by default, which can be changed to guild-wide by setting the scope to `guild`.
+   * @default undefined
+   */
+  rateLimit?: {
+    /**
+     * The number of times this command can be used in the given time period.
+     * @default 1
+     */
+    times?: number;
+    /**
+     * The time period in seconds.
+     * @default 5
+     */
+    period?: number;
+    /**
+     * The scope of the rate limit.
+     * @default user
+     *
+     * @values
+     * `user` - The rate limit will be on a per-user basis.
+     *
+     * `guild` - The rate limit will be on a per-guild basis.
+     */
+    scope?: 'user' | 'guild';
+  };
 };
 export type CommandExec = (props: CommandProps) => Awaitable<unknown>;
 export type CommandMeta =
