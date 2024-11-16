0ae9fcd6fb97c44500c6670bd2d95b6dacf78d60
Thijn <thijnkroon@gmail.com>
2024-11-16 13:58:35 +0100

    fix: move client login to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import client from 'client';
 import { initPaginators, registerEvents } from 'utils';
 import events from './events/index.ts';
+import ENV from 'env';
 
+client.login(ENV.BOT_TOKEN);
 registerEvents(client, events);
 initPaginators();
